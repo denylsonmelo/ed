7f2522ca73d2b447bf8f0496b5b788daebc29331
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2018-09-11 14:59:45 -0300

    mais exemplos de arrays multidimensional

--- src/main/java/br/edu/ifpi/capar/ed/ArrayMultidimensional.java
+++ src/main/java/br/edu/ifpi/capar/ed/ArrayMultidimensional.java
@@ -7,7 +7,9 @@ package br.edu.ifpi.capar.ed;
 public class ArrayMultidimensional {
 
     private static final int QUANT_ALUNOS = 5;
-    private static final int QUANT_NOTAS = 2;
+    private static final int QUANT_NOTAS = 4;
+    private static final int QUANT_MEDIAS = 2;
+    private static final int QUANT_NOTAS_MEDIA = 2;
 
     public static void main(String[] args) {
 
@@ -23,6 +25,8 @@ public class ArrayMultidimensional {
 
         notasAlunos[0][0] = 5;
         notasAlunos[0][1] = 7;
+        notasAlunos[0][2] = 8;
+        notasAlunos[0][3] = 5;
         notasAlunos[1][0] = 7;
         notasAlunos[1][1] = 6;
         notasAlunos[2][0] = 8;
@@ -31,29 +35,52 @@ public class ArrayMultidimensional {
         notasAlunos[3][1] = 2;
         notasAlunos[4][0] = 0;
         notasAlunos[4][1] = 8;
+        //      5               2
+        double mediaAlunos[][] = new double[QUANT_ALUNOS][QUANT_MEDIAS];
 
-
-        double mediaAlunos[][] = new double[QUANT_ALUNOS][1];
-        
-        int soma;
+        double notasProvisorias[][] = new double[QUANT_NOTAS_MEDIA][QUANT_NOTAS_MEDIA];
         for (int i = 0; i < QUANT_ALUNOS; i++) {
-            
-            soma = 0;
-            
-            for (int j = 0; j < QUANT_NOTAS; j++) {
+            for (int j = 0; j < QUANT_NOTAS_MEDIA; j++) {
+                for (int k = 0; k < QUANT_NOTAS; k++) {
 
-                soma += notasAlunos[i][j];
-                
+                    if( k >= 3)
+                    
+                    notasProvisorias[i][j] = notasAlunos[i][k];
+                }
+
+                mediaAlunos[i][j] = calcularMedia(notasProvisorias[j]);
             }
-            
-            mediaAlunos[i][0] = (double) soma/QUANT_NOTAS;
+
         }
 
-        
+        imprimirMedias(nomeAlunos, mediaAlunos);
+    }
+
+    private static void imprimirMedias(String[] alunos, double[][] medias) {
+
+        System.out.println("Nome\t Média 1 \t Média 2\t\t Média Final");
+
         for (int i = 0; i < QUANT_ALUNOS; i++) {
-            System.out.println(nomeAlunos[i] + ": " + mediaAlunos[i][0]);
+            System.out.print(alunos[i] + "\t   ");
+
+            for (int j = 0; j < QUANT_MEDIAS; j++) {
+                System.out.print(medias[i][j] + "\t\t   ");
+            }
+            System.out.print(calcularMedia(medias[i]));
+            System.out.println("");
         }
-        
+    }
+
+    private static double calcularMedia(double... valores) {
+        double media, soma = 0.0;
+
+        for (int i = 0; i < valores.length; i++) {
+            soma += valores[i];
+        }
+
+        media = soma / valores.length;
+
+        return media;
     }
 
 }
