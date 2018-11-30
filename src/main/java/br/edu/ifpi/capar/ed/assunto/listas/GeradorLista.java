7e23487736db7d35cc59ba912ce6c9a8f928b25a
Denylson Melo <143313+denylsonmelo@users.noreply.github.com>
2018-11-30 16:27:53 -0300

    adicionando grupos de trabalho

--- src/main/java/br/edu/ifpi/capar/ed/assunto/listas/GeradorLista.java
+++ src/main/java/br/edu/ifpi/capar/ed/assunto/listas/GeradorLista.java
@@ -11,30 +11,30 @@ import java.util.Random;
 public class GeradorLista {
 
     private static List<String> primeirosNomes = Arrays.asList(
-            "Algoritmos", "Fundamentos", "hardware", 
-            "Sistemas", "Seguranca", "introducao", 
-            "redes", "projeto", "estrutura", 
-            "programacao", "empreendedorismo", "topicos", 
-            "arquitetura", "ingles", "antropologia", 
-            "metodologia", "analise", "fisica", 
-            "eletromecanica", "calculo", "edificacoes", 
+            "Algoritmos", "Fundamentos", "hardware",
+            "Sistemas", "Seguranca", "introducao",
+            "redes", "projeto", "estrutura",
+            "programacao", "empreendedorismo", "topicos",
+            "arquitetura", "ingles", "antropologia",
+            "metodologia", "analise", "fisica",
+            "eletromecanica", "calculo", "edificacoes",
             "direito", "medicina", "biologia", "interface", "desenho", "banco"
     );
-    
+
     private static List<String> segundosNomes = Arrays.asList(
             "informatica", "basico", "operacionais",
             "informacao", "web", "mobile", "embarcado",
             "computadores", "supervisionado", "nao-supervisionado",
             "dados", "especiais", "diferencial"
     );
-    
+
     
     private static List<String> valores = Arrays.asList(
             "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
             "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"
     );
-    
-    public static Lista gerar(int total){
+
+    public static Lista gerar(int total) {
         System.out.println("   >>>    iniciando geracao de lista");
         long inicio = System.currentTimeMillis();
         Lista lista = new Lista();
@@ -43,28 +43,28 @@ public class GeradorLista {
             lista.adicionarNoFinal(gerarMateria());
         }
         long fim = System.currentTimeMillis();
-        System.out.println("   >>>    terminando geracao de lista. >> " + (fim-inicio) + " milisegundos");
+        System.out.println("   >>>    terminando geracao de lista. >> " + (fim - inicio) + " milisegundos");
         return lista;
     }
-    
-    private static Materia gerarMateria(){
+
+    private static Materia gerarMateria() {
         Random random = new Random();
-        
+
         int valorAleatorio = random.nextInt(primeirosNomes.size());
         String nome = primeirosNomes.get(valorAleatorio);
-    
+
         valorAleatorio = random.nextInt(segundosNomes.size());
         nome = nome + " " + segundosNomes.get(valorAleatorio);
-        
+
         valorAleatorio = random.nextInt(valores.size());
         nome = nome + " " + valores.get(valorAleatorio);
-        
+
         int cargaHoraria = random.nextInt(180);
         Materia materia = new Materia(nome, Integer.toString(cargaHoraria));
-        
+
         System.out.println(materia);
-        
+
         return materia;
     }
-    
+
 }
---
+++ src/main/java/br/edu/ifpi/capar/ed/trabalhos/trabalho 1.md
@@ -0,0 +1,25 @@
+[Sorting algorithm]
+
+insertion 2
+    - elias - savio
+
+bubble 3
+    - luzia - taynnar - iuri
+
+selection 3
+    - santiago - wendel - manuel
+
+quick 4
+    - gustavo - dionis - gilvan - henrique
+
+merge 4
+    - joyce - ze - claudemir - vitoria
+
+shell 5
+    - jailson - geyssy - fernanda - arthur - thaigo
+
+bucket 5
+    - davi - beatriz - valentina - kleiton - breno
+
+
+[Sorting algorithm]: https://en.wikipedia.org/wiki/Sorting_algorithm
